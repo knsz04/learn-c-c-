0c9c4fe36bba86212f940300d7b376e332c00a46
knsz04 <sumaiya8292sj@gmail.com>
2026-04-10 00:37:24 +0800

    config c,cpp

---
+++ cpp/learn/learn.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   /* int a,b;
+    cin >> a >> b;
+    cout << a << b << endl;*/
+    
+    float a = 2345678900.001;
+    float b = 3;
+    cout << fixed << a << endl;
+    float c = a + b;
+    cout << c << endl;
+    return 0;
+}
